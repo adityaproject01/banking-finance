5413c4ed3915af93316c897d7611bdf11076d0c3
aditya <aditya31031998@gmail.com>
2024-10-19 04:41:43 -0700

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-06b21ccaeff8cd686"
+  ami = "ami-0f71013b2c8bd2c29"
   instance_type = "t2.micro"
-  key_name = "mykey"
-  vpc_security_group_ids = ["sg-04cf4e3801bd09203"]
+  key_name = "docker"
+  vpc_security_group_ids = ["sg-04f83cc34c7a29093"]
   connection {
      type = "ssh"
      user = "ec2-user"
-     private_key = file("./mykey.pem")
+     private_key = file("./docker.pem")
      host = self.public_ip
      }
   provisioner "remote-exec" {
